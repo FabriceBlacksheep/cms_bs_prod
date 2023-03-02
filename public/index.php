b3584760fffc705e4d8e9ea21923bba5ea18e22c
FabriceBlacksheep <fabrice@blacksheep-van.com>
2023-03-02 13:20:36 +0100

    login

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request;
-
-
 
 
 
@@ -13,8 +9,3 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-Request::setTrustedProxies(
-    // trust *all* requests
-    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
-    Request::HEADER_X_FORWARDED_ALL
-);
