6fd3e4b1d32a07841c2c8ac115cd0a9ffbc0dd0e
FabriceBlacksheep <fabrice@blacksheep-van.com>
2023-03-03 15:10:38 +0100

    log

--- .env.local.php
+++ .env.local.php
@@ -8,6 +8,7 @@ return array (
   'TRUSTED_PROXIES' => '127.0.0.1,REMOTE_ADDR',
   'MESSENGER_TRANSPORT_DSN' => 'doctrine://default?auto_setup=0',
   // 'DATABASE_URL' => 'mysql://root@127.0.0.1:3306/bs_cms?serverVersion=mariadb-10.5.8',
+  'DATABASE_URL' => 'mysql://root:Ps14087940!@34.155.171.58:3306/blacksheep_cms?serverVersion=mariadb-10.5.8',
   'MAILER_DSN' => 'gmail://stackoverflow@gmail.com:admin123@default?verify_peer=0',
   'CORS_ALLOW_ORIGIN' => '^https?://(localhost|127\\.0\\.0\\.1)(:[0-9]+)?$',
   'CORS_ALLOW_CREDENTIALS' => 'true',
